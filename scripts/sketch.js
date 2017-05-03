e738e7154112874ec5e9cd64c88235a2bc29f33e
Mario <mariob360@gmail.com>
2017-05-03 16:12:52 -0700

    rainbow function now works

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
-	
-	
+
+
 	/*@desc amount of squares horizontally + vertically 
 	then uses a loop to add them into the container*/
 	var g_size = 16;
+	var darkColor = true;
+	var rgb = null;
 
 	for (i = 0; i < g_size * g_size; i++) {
 		var $row = $("<div class=sketch></div>");
@@ -12,25 +14,68 @@ $(document).ready(function () {
 
 	//@desc obtains the correct sizing for the squares being added in. 
 	var calc = 500 / g_size;
-	
+
 	$(".sketch").css({
 		"width": calc + "px"
 	});
 	$(".sketch").css({
 		'height': calc + "px"
 	});
-
+	
+	
+	var rainbowColorPicker = function () {
+		var r = Math.floor(Math.random() * 256);
+		var g = Math.floor(Math.random() * 256);
+		var b = Math.floor(Math.random() * 256);
+		rgb = "rgb(" + r + "," + g + "," + b + ")"
+		return rgb;
+	}
 
 	// @desc gives #303832 color when mouse touches div with a class of sketch
-	var colorChange = function () {
-		$(".sketch").on('mouseenter', function () {
-			$(this).addClass("sketch-active");
-		});
-	}
-	colorChange(); 
+	var colorDark = function () {
+			$(".sketch").on('mouseenter', function () {
+				$(this).addClass("sketch-active");
+			});
+		}
 
-	// @desc this removes the color that was added from colorChange
-	$(".remove").on('click', function () {
-		$("div").removeClass("sketch-active");
-	});
+	var rainbowColor = function () {
+			$(".sketch").on('mouseenter', function () {
+				$(this).css('background-color', rainbowColorPicker());
+			});
+		}
+
+
+		
+/*=======buttons====== */
+			
+			
+		$(".rainbow").on('click', function () {
+			darkColor = false;
+			rainbowColor();
+			colorManage();
+		});
+
+		// @desc this removes any colored blocks
+		$(".remove").on('click', function () {
+			$("div").removeClass("sketch-active");
+		});
+	
+		$(".reset").on('click', function(){
+			darkColor = true;
+			rgb = null;
+			colorManage();
+		});
+	
+	
+
+
+	var colorManage = function () {
+
+		if (rgb !== null && darkColor == false) {
+			rainbowColor();
+		} else {
+			colorDark();
+		}
+	}
+	colorManage();
 });
