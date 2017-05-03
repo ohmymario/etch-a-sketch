4f914570cb0792c09f39e17a9dd87822cf363948
Mario <mariob360@gmail.com>
2017-05-02 17:12:59 -0700

    grid now responds to any number of boxes

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,32 +1,36 @@
 $(document).ready(function () {
+	
+	
+	/*@desc amount of squares horizontally + vertically 
+	then uses a loop to add them into the container*/
+	var g_size = 16;
 
-	var g_size = 50;
-
-	for (i = 0; i < g_size*g_size; i++) {
+	for (i = 0; i < g_size * g_size; i++) {
 		var $row = $("<div class=sketch></div>");
 		$(".container").append($row);
 	}
 
-
+	//@desc obtains the correct sizing for the squares being added in. 
 	var calc = 500 / g_size;
 	
-
 	$(".sketch").css({
 		"width": calc + "px"
-
 	});
 	$(".sketch").css({
-		'height': calc + 'px'
-
+		'height': calc + "px"
 	});
 
-var colorChange = function(){
-	$(".sketch").on('mouseenter', function(){
-		$(this).css({'background-color': 'orange'});
+
+	// @desc gives #303832 color when mouse touches div with a class of sketch
+	var colorChange = function () {
+		$(".sketch").on('mouseenter', function () {
+			$(this).addClass("sketch-active");
+		});
+	}
+	colorChange(); 
+
+	// @desc this removes the color that was added from colorChange
+	$(".remove").on('click', function () {
+		$("div").removeClass("sketch-active");
 	});
-}
-
-colorChange();
-	
-
 });
